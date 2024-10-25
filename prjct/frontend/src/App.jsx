c70b4ecb2ae411f24d9143f0207f4e1e1ca8c22c
theeporiiii <122599080+theeporiiii@users.noreply.github.com>
2024-10-25 03:13:39 -0700

    Add files via upload

---
+++ prjct/frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import Signin from './components/Signin'
+import {  Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Signup from './components/Signup'
+import Hom from './components/Hom'
+import AddComputer from './components/AddComputer'
+
+
+
+
+function App() {
+
+
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/in' element={<Signin/>}/>
+      <Route path='/sign' element={<Signup/>}/>
+      <Route path='/hom' element={<Hom/>}/>
+      <Route path='/Add' element={<AddComputer/>}/>
+
+  
+
+    </Routes>
+  )
+}
+
+export default App
---
+++ prjct/frontend/src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='home1'>
+      <div>
+    <div className='content'>
+      <h1>COMPUTER LAB MANAGEMENT SYSTEM</h1>
+      <h3>GET STARTED</h3>
+
+
+      <div className='buttons'>
+      <a href="/in"><button className='btn sign-in'>Sign In</button></a>
+      <a href="/sign"><button className='btn sign-up'>Sign Up</button></a>
+      </div>
+    </div>
+    </div>
+  </div>
+  )
+}
+
+export default Home
+
